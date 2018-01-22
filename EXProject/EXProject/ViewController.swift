631e663c305f3bdb5958664082fc1c7ec5f62840
Naveen <pnaveen30@achiever-49.local>
2018-01-22 12:37:15 +0530

    a 40

--- EXProject/EXProject/ViewController.swift
+++ EXProject/EXProject/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         
         super.viewDidLoad()
         
-        let a = 20
+        let a = 40
         
         // Do any additional setup after loading the view, typically from a nib.
     }
