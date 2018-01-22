b315a980653ddd032fce811466473479afca405b
Naveen <pnaveen30@achiever-49.local>
2018-01-22 12:09:06 +0530

    I created Xcode project

---
+++ Pulse/Pulse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pulse
+//
+//  Created by Naveen on 1/22/18.
+//  Copyright © 2018 Naveen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
